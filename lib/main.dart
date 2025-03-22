b641632e7a06cff4d9c154177070399f864bbe50
Тушков Алексей Михайлович <a.tushkov@aitiguru.ru>
2025-03-22 16:47:59 +0300

    feat(lib main.dart): return code for image saving and editing

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,115 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:opencv_dart/opencv.dart' as cv;
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'styleGan/run.dart';
-import 'dart:io';
-import 'dart:async';
-
-
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: ImagePickerExample(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class ImagePickerExample extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _ImagePickerExampleState createState() => _ImagePickerExampleState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _ImagePickerExampleState extends State<ImagePickerExample> {
-  String imagePath = '';
+class _MyAppState extends State<MyApp> {
+  var images = <Uint8List>[];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<(cv.Mat, cv.Mat)> heavyTaskAsync(cv.Mat im, {int count = 1000}) async {
+    late cv.Mat gray, blur;
+    for (var i = 0; i < count; i++) {
+      gray = await cv.cvtColorAsync(im, cv.COLOR_BGR2GRAY);
+      blur = await cv.gaussianBlurAsync(im, (15, 15), 5, sigmaY: 5);
+      if (i != count - 1) {
+        gray.dispose(); // manually dispose
+        blur.dispose(); // manually dispose
+      }
+    }
+    return (gray, blur);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final String assetsImagePath = 'assets/images/smith_aligned.jpg';
-
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text('CONV'),
-        backgroundColor: Colors.grey[900],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // final ImagePicker picker = ImagePicker();
-                // final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-                // if (image != null) {
-                //   await saveImageToDirectory(image.path);
-                //   setState(() {
-                //     imagePath = assetsImagePath;
-                //   });
-                // }
-
-                main();
-
-              },
-              child: const Text('Применить'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset(
-                  assetsImagePath,
-                  fit: BoxFit.cover,
-              )
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Native Packages'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  final picker = ImagePicker();
+                  final img = await picker.pickImage(source: ImageSource.gallery);
+                  if (img != null) {
+                    final path = img.path;
+                    final mat = cv.imread(path);
+                    print("cv.imread: width: ${mat.cols}, height: ${mat.rows}, path: $path");
+                    debugPrint("mat.data.length: ${mat.data.length}");
+                    // heavy computation
+                    final (gray, blur) = await heavyTaskAsync(mat, count: 1);
+                    setState(() {
+                      images = [
+                        cv.imencode(".png", mat).$2,
+                        cv.imencode(".png", gray).$2,
+                        cv.imencode(".png", blur).$2,
+                      ];
+                    });
+                  }
+                },
+                child: const Text("Выбрать изображение"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final data = await DefaultAssetBundle.of(context).load("assets/images/smith_aligned.jpg");
+                  final bytes = data.buffer.asUint8List();
+                  // Тяжелые вычисления о этого способа
+                  // final (gray, blur) = await heavyTask(bytes);
+                  // setState(() {
+                  //   images = [bytes, gray, blur];
+                  // });
+                  final (gray, blur) = await heavyTaskAsync(cv.imdecode(bytes, cv.IMREAD_COLOR));
+                  setState(() {
+                    images = [bytes, cv.imencode(".png", gray).$2, cv.imencode(".png", blur).$2];
+                  });
+                },
+                child: const Text("По умолчанию"),
+              ),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: images.length,
+                        itemBuilder: (ctx, idx) => Card(
+                          child: Image.memory(images[idx]),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  Future<void> saveImageToDirectory(String imagePath) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final newDirectory = Directory('${directory.path}/styleGan/notebook');
-
-    if (!await newDirectory.exists()) {
-      await newDirectory.create(recursive: true);
-    }
-
-    final imageFile = File(imagePath);
-    final newImagePath = '${newDirectory.path}/${imageFile.uri.pathSegments.last}';
-    await imageFile.copy(newImagePath);
-
-    print('Image saved to $newImagePath');
   }
 }
